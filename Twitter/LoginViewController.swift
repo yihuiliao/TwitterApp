8b47def8f4ead251097656452b2e001c4b54bd3d
Yihui Liao <liao.yihui18@gmail.com>
2020-10-07 18:55:25 -0400

    user will stay logged in unless they logout

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,9 +16,18 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+        
+    }
+    
     @IBAction func loginButton(_ sender: Any) {
         let url = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: url, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("failed to login")
